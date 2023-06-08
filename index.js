469e4c674e11649aa7c7bb6e284189a5a962d51a
Qianyun Mu <msbluemu@gmail.com>
2023-06-08 18:05:50 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9\-\'\s]+/g, '')
+  }
+ 
+  static titleize(string){
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    const words = string.toLowerCase().split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !excludedWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+    }
+  }
